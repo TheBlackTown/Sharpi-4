c6860d86eccca21937e186c1b18f4d75d6de824b
TheBlackTown <70762240+TheBlackTown@users.noreply.github.com>
2020-09-12 11:32:37 +0300

    Add files via upload

---
+++ Sharpi 4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Проверка_неявного_преобразования
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int16 i16 = 1;
+            Int32 i32 = 1;
+            double db = 1;
+
+            i16 = 132;
+            i16 = (short) db;
+            i32 = i16;
+            i32 = (int) db;
+            db = 116;
+            db = i32;
+        }
+    }
+}
